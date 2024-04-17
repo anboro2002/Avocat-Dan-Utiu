9eef07c0daec2f5dcb685b63e22ddfd47f82c5dd
AndreiBorsos <andrei.borsos@dotsquare.ro>
2024-04-17 14:08:25 +0300

    Added the navbar and Services

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,4 @@
+// hamburger menu pt mobile si tablet
 const hamMenu = document.querySelectorAll(".ham-menu");
 const offScreen = document.querySelectorAll(".off-screen-menu");
 
@@ -7,3 +8,23 @@ hamMenu.forEach((hamMenu,index) =>{
         offScreen[index].classList.toggle("active");
     });
 });
+
+// dropdown menu pt servicii in desktop
+
+const dropDownToggle = document.querySelector(".dropdown-toggle");
+const dropDownMenu = document.querySelector(".dropdown-menu");
+
+// deschide meniul la hover
+    dropDownToggle.addEventListener("mouseenter", () =>{
+        dropDownToggle.classList.toggle("show");
+        dropDownMenu.classList.toggle("show");
+    });
+
+
+
+// inchide meniul la click in afara meniului
+    document.body.addEventListener("click", (event) => {
+        if(!dropDownMenu.contains(event.target)){
+            dropDownMenu.classList.remove("show");
+        }
+    });
